d38f0c7e7db687fa19164db7e49ffe98fdcdd6ed
Fabian Petersen <fabian@webrus.co.za>
2022-12-31 17:39:45 +0200

    socials-updated

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -40,52 +40,12 @@ const Wrapper = styled.div`
   .social-icon {
     transition: all 0.15s ease-in;
     color: var(--clr-white);
-    border: var(--border-red);
   }
 
   .social-icon:hover {
     transform: scale(1.1);
     color: var(--clr-yellow);
     transform: translateY(-2.5px);
-  }
-
-  li {
-    position: relative;
-  }
-
-  .icon::after,
-  .icon::before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    border-radius: 100%;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    /* background: blue; */
-  }
-
-  .icon::after {
-    background-color: white;
-    animation: pulse 1000ms infinite;
-  }
-
-  .social-icon::before {
-    /* background-color: white; */
-  }
-
-  @keyframes pulse {
-    0% {
-      transform: scale(1);
-      opacity: 0;
-    }
-
-    100% {
-      transform: scale(1.3);
-      opacity: 1;
-    }
   }
 `;
 
